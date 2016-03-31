56943686918d1d0717b3067dcf68b413e7e7dff9
ManarAhmed <eng.mema2014@yahoo.com.au>
2016-03-31 19:36:59 +0200

    edit the database

--- zend_project.sql
+++ zend_project.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 30, 2016 at 07:32 PM
+-- Generation Time: Mar 31, 2016 at 08:34 PM
 -- Server version: 5.5.47-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -19,6 +19,171 @@ SET time_zone = "+00:00";
 --
 -- Database: `zend_project`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carrequest`
+--
+
+CREATE TABLE IF NOT EXISTS `carrequest` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `source` varchar(200) NOT NULL,
+  `destination` varchar(200) NOT NULL,
+  `date_from` varchar(200) NOT NULL,
+  `date_to` varchar(200) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  KEY `id` (`id`,`user_id`),
+  KEY `city_id` (`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE IF NOT EXISTS `city` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `rate` float NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `country_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `country_id` (`country_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city_rate`
+--
+
+CREATE TABLE IF NOT EXISTS `city_rate` (
+  `user_id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `value` float NOT NULL,
+  KEY `user_id` (`user_id`,`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `experience_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`,`experience_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city` varchar(200) NOT NULL,
+  `rate` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country_rate`
+--
+
+CREATE TABLE IF NOT EXISTS `country_rate` (
+  `user_id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `value` float NOT NULL,
+  KEY `user_id` (`user_id`,`country_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experience`
+--
+
+CREATE TABLE IF NOT EXISTS `experience` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `content` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotel`
+--
+
+CREATE TABLE IF NOT EXISTS `hotel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `city_id` (`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotelreserve`
+--
+
+CREATE TABLE IF NOT EXISTS `hotelreserve` (
+  `hotel_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date_from` varchar(200) NOT NULL,
+  `date_to` varchar(200) NOT NULL,
+  `hotel_name` varchar(200) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  UNIQUE KEY `user_id` (`user_id`),
+  KEY `hotel_id` (`hotel_id`,`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `city_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE IF NOT EXISTS `room` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hotel_id` int(11) NOT NULL,
+  `available` enum('1','0') NOT NULL,
+  `bed_num` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `hotel_id` (`hotel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -43,6 +208,16 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `type`, `is_active`) VALUES
 (1, 'manar', 'manar@yahoo.com', '123', '1', '1'),
 (2, 'aya', 'aya@yahoo.com', '123', '0', '1');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `city`
+--
+ALTER TABLE `city`
+  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
