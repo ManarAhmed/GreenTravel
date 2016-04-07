b711a6975f57045e75f0ac65f5f4e3f46467ef18
ayaOS <ayaali35.aa@gmail.com>
2016-04-07 22:01:28 +0200

    add bootstrap to addpost.form

--- application/forms/Addpost.php
+++ application/forms/Addpost.php
@@ -6,16 +6,22 @@ class Application_Form_Addpost extends Zend_Form
     public function init()
     {
         $this->setMethod('POST');
+        $this->setAttrib('class','form-horizontal');
+
         $title= new Zend_Form_Element_Text("title");
         $title->setLabel("Title : ");
+        $title->setAttrib('class','form-control');
 
         $content= new Zend_Form_Element_Textarea("content");
         $content->setLabel("Post Content: ");
         $title->setRequired();
         $content->setRequired();
+        $content->setAttrib('class','form-control');
+        $content->setAttribs(array('cols'=>'5', 'rows'=>'10'));
 
         $submit=new Zend_Form_Element_Submit("submit");
         $submit->setValue("Add Experience");
+        $submit->setAttrib('class','btn btn-success');
 
         $this->addElements( array(
                 $title,
